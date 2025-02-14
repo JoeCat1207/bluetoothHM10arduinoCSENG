63b3ac830924567e569c6948ff0d51c0eab7b296
ChilliCat <36356602+JoeCat1207@users.noreply.github.com>
2025-02-14 13:52:40 -0500

    code added

--- thebluetoothmodulehasbeenconnected.ino
+++ thebluetoothmodulehasbeenconnected.ino
@@ -4,6 +4,7 @@ const int LEDred = 2;
 const int LEDgreen = 4;
 const int LEDyellow = 6;
 #include <SoftwareSerial.h>
+char davis;
 
 SoftwareSerial tooth(TX_Pin, RX_Pin);
 
@@ -11,11 +12,31 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println("Serial Works!");
-  BTSerial.begin(9600);
+  tooth.begin(9600);
+  pinMode(LEDred, OUTPUT);
+  pinMode(LEDgreen, OUTPUT);
+  pinMode(LEDyellow, OUTPUT);
 
 }
 
 void loop() {
+
+  if (tooth.available() >0){
+    davis = tooth.read();
+    tooth.print("reading new input");
+    tooth.println(davis);
+
+  }
+
+  if(davis == 'on'){
+    Serial.println("davis has value!");
+    digitalWrite(LEDred, HIGH);
+  }
+  if(davis == 'off'){
+    Serial.println("turn off!");
+    digitalWrite(LEDred, LOW);
+  }
+  delay(100);
   // put your main code here, to run repeatedly:
 
 }
